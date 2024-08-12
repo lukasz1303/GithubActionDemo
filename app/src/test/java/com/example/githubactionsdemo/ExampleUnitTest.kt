a1317793d0880225f70102df4139ca4a8b0c478c
Łukasz Frątczak <lukasz_fratczak@wp.pl>
2024-08-12 23:59:48 +0200

    Remove failing test

--- app/src/test/java/com/example/githubactionsdemo/ExampleUnitTest.kt
+++ app/src/test/java/com/example/githubactionsdemo/ExampleUnitTest.kt
@@ -14,9 +14,4 @@ class ExampleUnitTest {
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
     }
-
-    @Test
-    fun addition_isCorrect_fail() {
-        assertEquals(5, 2 + 2)
-    }
 }
